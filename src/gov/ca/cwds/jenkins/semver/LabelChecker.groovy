7470b5e76268f2c2990baba90d3d918f03e0c3d6
Richard Bach <rich@ontargetcomputing.net>
2020-08-06 11:35:00 -0700

    fix

--- src/gov/ca/cwds/jenkins/semver/LabelChecker.groovy
+++ src/gov/ca/cwds/jenkins/semver/LabelChecker.groovy
@@ -37,12 +37,12 @@ class LabelChecker {
     labels
   }
 
-  def getPRLabels(projectName, credentials) {
+  def getPRLabels(projectName, stuff) {
     def get = new URL("https://api.github.com/repos/ca-cwds/${projectName}/issues/${script.env.ghprbPullId}/labels").openConnection();
-    get.setRequestProperty ("Authorization", "token: 95bc969a8a27b7c8987e6bb82e164e61e35f6cbd")
+    get.setRequestProperty ("Authorization", "token: 0affafa3c9e9b2aa4fe7f3b976e8a33478113586")
 
     get.setRequestProperty ("User-Agent", "cwds/1.0 ( jenkins )")
-    script.echo "******************I AM HERE with ${credentials}"
+    script.echo "******************I AM HERE with ${stuff}"
     def response = get.getInputStream().getText()
     script.echo( "THE RESPONSE IS ${response}" )
     def labels = script.readJSON(text: response)*.name
